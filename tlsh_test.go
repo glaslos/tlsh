2725cfccfae1a5127b484302424a01670adbdab0
Dalibor Dukic <dalibor.dukic@gmail.com>
2017-05-12 00:03:56 +0200

    Add table driven tests

--- tlsh_test.go
+++ tlsh_test.go
@@ -2,24 +2,29 @@ package tlsh
 
 import (
 	"fmt"
-	"os/exec"
 	"testing"
+)
+
+var (
+	testCases = []struct {
+		filename string
+		hash     string
+	}{
+		{"tests/test_file_1", "8ED02202FC30802303A002B03B33300FC30A82F83008C2FA000A0080B8BA0E02CCA0C3"},
+	}
 )
 
 func TestSomething(t *testing.T) {
 }
 
 func TestReal(t *testing.T) {
-	foo, err := exec.Command("tlsh", "-f", "LICENSE").Output()
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Printf("%s", foo)
-	if bar, err := Hash("LICENSE"); bar != string(foo[:]) {
-		fmt.Printf("%s", bar)
-		if err != nil {
-			t.Error(err)
+	for _, tc := range testCases {
+		if bar, err := Hash(tc.filename); bar != tc.hash {
+			fmt.Printf("%s\n", bar)
+			if err != nil {
+				t.Error(err)
+			}
+			t.Errorf("\nfilename: %s\n%s\n%s - doesn't match real hash\n", tc.filename, tc.hash, bar)
 		}
-		t.Error("Calculated hash doesn't match real hash")
 	}
 }
