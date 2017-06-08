3dc10e03b912fdb3171cdb010da47934d9ca14f2
glaslos <lukas_rist@symantec.com>
2017-06-08 17:16:55 +0200

    Makefile for release

---
+++ Makefile
@@ -0,0 +1,13 @@
+VERSION := v0.1.0
+BUILDSTRING := $(shell git log --pretty=format:'%h' -n 1)
+VERSIONSTRING := tlsh version $(VERSION)+$(BUILDSTRING)
+BUILDDATE := $(shell date -u -Iseconds)
+
+clean:
+	rm -rf dist/
+
+LDFLAGS := "-X \"main.VERSION=$(VERSIONSTRING)\" -X \"main.BUILDDATE=$(BUILDDATE)\""
+
+.PHONY: build_release
+build_release: clean
+	cd app; gox -arch="amd64" -os="windows darwin linux" -output="../dist/tlsh-{{.Arch}}-{{.OS}}" -ldflags=$(LDFLAGS)
--- app/tlsh.go
+++ app/tlsh.go
@@ -8,10 +8,22 @@ import (
 	"github.com/glaslos/tlsh"
 )
 
+var (
+	// VERSION is set by the makefile
+	VERSION = "v0.0.0"
+	// BUILDDATE is set by the makefile
+	BUILDDATE = ""
+)
+
 func main() {
 	var file = flag.String("f", "", "path to the file to be hashed")
 	var raw = flag.Bool("r", false, "set to get only the hash")
+	var version = flag.Bool("version", false, "print version")
 	flag.Parse()
+	if *version {
+		fmt.Printf("%s %s\n", VERSION, BUILDDATE)
+		return
+	}
 	if *file == "" {
 		fmt.Fprintf(os.Stderr, "Usage of %s [-f <file>]\n\n", os.Args[0])
 		flag.PrintDefaults()
